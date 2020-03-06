929023e61c44ffaf254e9e535e3acfe9a7088736
mirko93 <skopljak.mirko93@gmail.com>
2020-03-06 15:25:34 +0100

    "added phpmailer

---
+++ requestReset.php
@@ -0,0 +1,54 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if (isset($_POST['email'])) {
+
+    $emailTo = $_POST['email'];
+
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'ucenjemirko@gmail.com';                     // SMTP username
+        $mail->Password   = 'ucenjepassword';                               // SMTP password
+        $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        //Recipients
+        $mail->setFrom('ucenjemirko@gmail.com', 'Test');
+        $mail->addAddress($emailTo);     // Add a recipient
+        $mail->addReplyTo('no-reply@gmail.com', 'No reply');
+
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
+
+?>
+
+<form method="POST">
+    <input type="text" name="email" placeholder="Email" autocomplete="off">
+    <br>
+    <input type="submit" name="submit" value="Reset email">
+</form>
