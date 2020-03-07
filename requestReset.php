43cb462b1ec45497072cfe2472b95c5510a0e1ac
mirko93 <skopljak.mirko93@gmail.com>
2020-03-07 13:38:05 +0100

    "edit"

--- requestReset.php
+++ requestReset.php
@@ -34,13 +34,13 @@ if (isset($_POST['email'])) {
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'ucenjemirko@gmail.com';                     // SMTP username
-        $mail->Password   = 'ucenjepassword';                               // SMTP password
+        $mail->Username   = 'email@gmail.com';                     // SMTP username
+        $mail->Password   = 'password';                               // SMTP password
         $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
-        $mail->setFrom('ucenjemirko@gmail.com', 'Test');
+        $mail->setFrom('email@gmail.com', 'Test');
         $mail->addAddress($emailTo);     // Add a recipient
         $mail->addReplyTo('no-reply@gmail.com', 'No reply');
 
